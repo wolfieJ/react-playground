998bb7480fc2bc3249657c635045314bc0515a4f
Bala Jayachandran <Home@blackbird.local>
2020-03-28 14:17:27 -0400

    useState - counters exmaple

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,  { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [count, updateCount] = useState({count1: 10, count2: 20});
+  const { count1, count2 } = count;
+  const incrementOnClick = () => updateCount(currentState => ({...currentState, count1: currentState.count1 + 1, count2: currentState.count2 + 10}));
+  const decrementOnClick = () => updateCount(currentState => ({...currentState, count1: currentState.count1 - 1, count2: currentState.count2 - 10}));
+
+  return (    
+  <>
+    <button onClick={() => incrementOnClick()}>Increment + </button>
+    <button onClick={() => decrementOnClick()}>Decrement - </button>
+    <div className="CountersSpacing">
+      <div>Count 1: {count1}</div>
+      <div>Count 2: {count2}</div>
     </div>
-  );
+  </>)
 }
 
 export default App;
